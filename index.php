3e26057c64aa5daec48a06c92ff6833bcfa4449a
Crispin Manda <crispinmanda06@gmail.com>
2025-02-27 10:03:51 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+
+<?php 
+
+// variables
+
+$x = 10;
+
+echo '<br>' . var_dump($x) . 'we have used var_dump'; '</br>';
+
+
+$y = 20;
+
+$total = $x + $y;
+
+// output
+echo " the total is :" . ' '.  $total . '<br>';
+
+$name = "John"; '<br>';
+echo var_dump($name); '<br>';
+
+echo '<br>'. $name;
+
+$age = 35;
+
+echo '<br>'. $age;
+
+$fruit = array ('apple', 'banana', 'orange');
+
+echo '<br>'. $fruit[0] . ' ' . $fruit[1] . ' ' . $fruit[2] ;
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+     $name = 'johndoe';
+
+    echo ' <h1>This is a variable  </h1>'. $name 
+    ?>
+    
+
+</body>
+</html>
