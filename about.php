020c437a2a1f61aa27b24edd0136507fc9cfc845
Crispin Manda <crispinmanda06@gmail.com>
2025-03-06 11:20:55 +0300

    added css concept files

--- about.php
+++ about.php
@@ -1,5 +1,12 @@
 <?php include 'header.php'; ?>
 <?php include 'navbar.php'; ?>
 <h1> ABOUT PAGE</h1>
+<div class="container">
+    <div class="dashboard">
+        <p class="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam nobis saepe molestiae enim eligendi odio labore quod, ipsa maxime vitae tempora quia cum earum velit reiciendis est iusto quis adipisci.
+        </p>
+    </div>
+</div>
 
 <?php include 'footer.php'; ?>
--- index.php
+++ index.php
@@ -3,6 +3,9 @@ include 'navbar.php'; ?>
 
 
 <h1> welcome Home</h1>
+<p >
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ex corporis autem repellendus optio quasi deleniti eligendi fugit quis aliquam, laudantium minus eos numquam praesentium accusamus doloremque sit. Soluta, sed.
+</p>
 
 
 
